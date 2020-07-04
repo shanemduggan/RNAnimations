ed5ef5796bab35dc6ddd25db8975a7a5fc15f42f
Shane Duggan <devshaneduggan@gmail.com>
2020-07-04 08:17:19 -0700

    Minor

---
+++ screens/class/StaggeredHeads.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  View,
+  Dimensions,
+  TouchableWithoutFeedback,
+  Animated
+} from "react-native";
+
+export default class StaggeredHeads extends Component {
+  state = {
+
+  }
+
+  startAnimation = () => {
+
+  }
+
+  render() {
+    return (
+      <View>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
